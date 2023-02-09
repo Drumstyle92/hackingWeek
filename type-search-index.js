1bd5d65f50efc68456ae7d1a4a002b299b884463
Dino <dino15031992@gmail.com>
2023-02-10 00:25:49 +0100

    commit

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"it.bibloteca.develhope","l":"Libro"},{"p":"it.bibloteca.develhope","l":"Test"},{"p":"it.bibloteca.develhope","l":"UtenteBiblioteca"}];updateSearchResults();
